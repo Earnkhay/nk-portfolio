51cefc8d445473d8db34fab2190e2a55f2b6a4bc
Nkechi Julia <nkechionyimadu@gmail.com>
2022-08-25 20:45:48 +0100

    testing endpoint again

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('contact-form')
+
+form.addEventListener('submit', formSubmit);
+
+
+function formSubmit(e){
+    e.preventDefault();
+    //read the value
+    let name = document.getElementById('contact-name');
+    let phone = document.getElementById('contact-phone');
+    let email = document.getElementById('contact-email');
+    let subject = document.getElementById('subject');
+    let message = document.getElementById('contact-message');
+
+    const formObject = {
+        name: name.value,
+        phone: phone.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value
+    }
+
+    //fetch request
+    fetch('https://vue-http-learning-b7e81-default-rtdb.firebaseio.com/portfolio.json', {
+        method: 'POST',
+        headers: {
+          //tells server we'll add data to the request which would be in json format
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify( {
+          formObject: "formObject"
+        }),
+      });
+}
